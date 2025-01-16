5d28f43a4c158c1d89fbce7c4d36d9be82bf9db6
Gayana <gsdallakian@edu.hse.ru>
2025-01-16 18:02:17 +0300

    поднятие и подключение базы данных

--- cmd/main.go
+++ cmd/main.go
@@ -5,15 +5,32 @@ import (
 	"github.com/Gayana5/todo-app/pkg/handler"
 	"github.com/Gayana5/todo-app/pkg/repository"
 	"github.com/Gayana5/todo-app/pkg/service"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 	"github.com/spf13/viper"
 	"log"
+	"os"
 )
 
 func main() {
 	if err := initConfig(); err != nil {
 		log.Fatalf("error initializing configs: %s", err.Error())
 	}
-	repos := repository.NewRepository()
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("error loading .env variables: %s", err.Error())
+	}
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+		Password: os.Getenv("DB_PASSWORD"),
+	})
+	if err != nil {
+		log.Fatalf("error initializing DataBase: %s", err.Error())
+	}
+	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
@@ -21,6 +38,7 @@ func main() {
 	if err := srv.Run(viper.GetString("8000"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
+
 }
 func initConfig() error {
 	viper.AddConfigPath("configs")

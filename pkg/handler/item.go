6e93043d949316e4ca92309803b3e61700fc193c
Gayana <gsdallakian@edu.hse.ru>
2025-01-16 20:16:09 +0300

    реализация регистрации

--- pkg/handler/item.go
+++ pkg/handler/item.go
@@ -5,9 +5,7 @@ import "github.com/gin-gonic/gin"
 func (h *Handler) createItem(c *gin.Context) {
 
 }
-func (h *Handler) getAllItems(c *gin.Context) {
-
-}
+func (h *Handler) getAllItems(c *gin.Context) {}
 func (h *Handler) getItemById(c *gin.Context) {
 
 }
--- pkg/service/service.go
+++ pkg/service/service.go
@@ -1,8 +1,12 @@
 package service
 
-import "github.com/Gayana5/todo-app/pkg/repository"
+import (
+	"github.com/Gayana5/todo-app"
+	"github.com/Gayana5/todo-app/pkg/repository"
+)
 
 type Authorization interface {
+	CreateUser(user todo.User) (int, error)
 }
 type TodoList interface {
 }
@@ -16,5 +20,7 @@ type Service struct {
 }
 
 func NewService(repos *repository.Repository) *Service {
-	return &Service{}
+	return &Service{
+		Authorization: NewAuthService(repos.Authorization),
+	}
 }
